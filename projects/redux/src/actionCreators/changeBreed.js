895f161cfbce0c46cdbf46e447b7e5e2e6e6f6ad
Bohdan Skochii <b.skochii@gmail.com>
2022-07-28 14:32:01 +0300

    add useState

--- projects/redux/src/actionCreators/changeBreed.js
+++
@@ -1,3 +0,0 @@
-export default function changeBreed(location) {
-  return { type: "CHANGE_BREED", payload: location };
-}
--- src/Carousel.js
+++ src/Carousel.js
@@ -9,26 +9,28 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
-  handleClick = (event) => {
-    this.setState({ active: +event.target.dataset.index });
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
   };
 
   render() {
     const { active } = this.state;
     const { images } = this.props;
-
     return (
       <div className="carousel">
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
-              onClick={this.handleClick}
-              src={photo}
               key={photo}
-              data-index={index}
+              src={photo}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
             />
           ))}
         </div>
